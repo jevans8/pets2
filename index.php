55681c7a672bf40df716d92513407ab0013de284
Julia Evans <jevans8@mail.greenriver.edu>
2020-05-01 16:58:13 -0400

    Summary page added

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+//Start a session
+session_start();
+
 //Require the autoload file
 require_once('vendor/autoload.php');
 
@@ -20,6 +23,46 @@ $f3->route('GET /', function(){
 
     //display home page via render method
     echo $view->render('views/pet-home.html');
+
+});
+
+//Order route
+$f3->route('GET|POST /order', function($f3){
+
+    //check if the form has been posted
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        //echo "post method";
+
+        //validate the data
+        if(empty($_POST['pet'])){
+            echo "please supply a pet type";
+        }
+        else{
+            //data is valid
+            $_SESSION['pet'] = $_POST['pet'];
+            $_SESSION['color'] = $_POST['color'];
+
+            //redirect to the summary route
+            $f3->reroute("summary");
+        }
+    }
+
+    //instantiate new template object
+    $view = new Template();
+
+    //display home page via render method
+    echo $view->render('views/pet-order.html');
+
+});
+
+//Order summary route
+$f3->route('GET /summary', function(){
+
+    //instantiate new template object
+    $view = new Template();
+
+    //display home page via render method
+    echo $view->render('views/order-summary.html');
 
 });
 
